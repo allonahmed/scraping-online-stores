044a04669c28097e00056af34dc1fc05eb85a9e7
allonahmed <allonahmed@mec.science>
2022-09-29 15:58:11 -0400

    more parsing ex

--- test-scrape/fake_jobs.py
+++ test-scrape/fake_jobs.py
@@ -24,24 +24,30 @@ def getSiteContent(url):
 
 HTML_content = getSiteContent(SITE_URL)
 
-#create beautiful soup object for parsing web content
+# create beautiful soup object for parsing web content
 soup = BeautifulSoup(HTML_content, 'html.parser') 
 
-# we can navigate through the html content through the find and find_all method which 
-# creates new BeautifulSoup objects 
-results_container = soup.find(id = 'ResultsContainer')
-card_content = results_container.find_all('div', class_ = 'card-content')
-
-#example for extracting all images from page
+# example for extracting all images from page
 def getImageSource():
   images = soup.find_all('img')
   results = []
   for index, image in enumerate(images, start = 1):
     print('image {}: {}'.format(index, image['src']))
 
-getImageSource()
+# example function of parsing content and getting specific data from html
+def getContentCardInformation():
+  # we can navigate through the html content through the find and find_all method which 
+  # creates new BeautifulSoup objects 
+  results_container = soup.find(id = 'ResultsContainer')
+  card_content = results_container.find_all('div', class_ = 'card-content')
 
-# for card in card_content:
-#   print(card.prettify(), end = '\n'*3)  
+  # further parsing of each 'card-content' element and accessing the text of the element
+  # strip() is useful for removing whitespace that may be generated from the html to string conversion
+  for card in card_content:
+    title_element = card.find('h2', class_ = 'title')
+    company_element = card.find('h3', class_ = 'company')
+    location_element = card.find('p', class_ = 'location')
+    print(f'title:    {title_element.text.strip()}')
+    print(f'company:  {company_element.text.strip()}')
+    print(f'location: {location_element.text.strip()}', end = '\n'*2)    
 
-#
