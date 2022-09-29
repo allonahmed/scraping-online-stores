b677e63bfc8e812e8e1c349559dacb95a29aa597
allonahmed <allonahmed@mec.science>
2022-09-29 14:58:23 -0400

    example of parsing html content using beautiful soap

--- test-scrape/fake_jobs.py
+++ test-scrape/fake_jobs.py
@@ -1,6 +1,8 @@
 import requests
 from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
 
+# ***NOTE*** this demo is intended for static websites only
 
 #site url of demo site we want to scrape
 SITE_URL = 'https://realpython.github.io/fake-jobs/'
@@ -18,6 +20,14 @@ def getSiteContent(url):
     return { 'Exception message': f'Other error occurred: {err}' }
 
   else:
-    return response.text
+    return response.content
 
-print(getSiteContent(SITE_URL))
+HTML_content = getSiteContent(SITE_URL)
+
+soup = BeautifulSoup(HTML_content, 'html.parser')
+
+results_container = soup.find(id = 'ResultsContainer')
+card_content = results_container.find_all('div', class_ = 'card-content')
+
+for card in card_content:
+  print(card.prettify(), end = '\n'*3)
