eeaab2e2620b8f1fa5beaeeb6415df6c2694cd5d
allonahmed <allonahmed@mec.science>
2022-10-01 16:55:11 -0400

    added time module for adding delays if neccessary

--- selenium-test/main.py
+++ selenium-test/main.py
@@ -21,21 +21,23 @@ driver.get(URL)
 def display_page_source():
   assert driver.page_source, 'No results found'
   return driver.page_source
-# print('page source: ', display_page_source())
+print('page source: ', display_page_source())
 # access the website title
 print('page title: ', driver.title)
 # acces the current URL (could be useful in the case of redirects)
 print('page current URL: ', driver.current_url)
 
+# to add delay (useful for waiting for server/api loading)
+time.sleep(3)
+
 # using the find_element method to find elements. You can search by name, class, etc
 # more details: https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
-time.sleep(1)
-
 elem = driver.find_elements(By.ID, "product-schema")
 for i in elem: 
   print(i.get_attribute('innerHTML'))
 
 print('amount of items:', len(elem))
+
 # now we can send keys (like entering keys on our keyboard)
 # first we will clear any prepoluated text (just in case), then we will pass in the query we want, and then return it
 # elem.clear()
