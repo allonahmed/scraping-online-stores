162d341a0291d7e79ff9228c9f7d5e8d03611633
allonahmed <allonahmed@mec.science>
2022-10-01 19:54:10 -0400

    udated test files

--- selenium-test/main.py
+++ selenium-test/main.py
@@ -2,17 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 import time 
-
 
 # instantiate options to be passed to chrome web driver
 options = Options()
 options.headless = False # setting this to true to avoid opening browser gui
 options.add_argument("--window-size=1900,1200") # additional arguments 
 options.add_argument("--auto-open-devtools-for-tabs");
-
 
 # connect instance of Chrome webdriver
 driver = webdriver.Chrome(options = options, executable_path = '../../../Downloads/chromedriver')
@@ -24,33 +20,11 @@ driver.get(URL)
 def display_page_source():
   assert driver.page_source, 'No results found'
   return driver.page_source
-# print('page source: ', display_page_source())
+print('page source: ', display_page_source())
 # access the website title
 print('page title: ', driver.title)
 # acces the current URL (could be useful in the case of redirects)
 print('page current URL: ', driver.current_url)
-
-
-# wait = WebDriverWait(driver, 10)
-# popup = wait.until(ec.visibility_of_element_located(('xpath', '/html/body/div[2]/div/main/div/div/div/div[1]/div/form/div[2]/button[2]')))
-
-time.sleep(5)
-iframe = driver.find_element('xpath', '//*[@id="attentive_creative"]')
-driver.switch_to.frame(iframe)
-
-driver.find_element('xpath', '/html/body/div[2]/div/main/div/div/div/div[1]/div/form/div[2]/button[2]').click()
-
-driver.switch_to.default_content()
-
-time.sleep(3)
-# accessing load button so we can reach end of page before getting all of our data
-lm_button = driver.find_element('xpath' ,'//*[@id="portal-collection"]/section/button')
-
-for i in range(0,11):
-  time.sleep(2)
-  driver.execute_script("arguments[0].scrollIntoView();", lm_button)
-  print('clicked')
-  lm_button.click()
 
 #to add delay (useful for waiting for server/api loading)
 time.sleep(3)
@@ -70,5 +44,5 @@ print('amount of items:', len(elem))
 # elem.send_keys(Keys.RETURN)
 
 # exit the browser (could use close method to close the single tab that was open)
-# driver.quit()
+driver.quit()
 
