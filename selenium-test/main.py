4ceffc6148f108c5ba64bc78a87b519eb0acc4b3
allonahmed <allonahmed@mec.science>
2022-10-01 19:46:14 -0400

    successfully able to scrape store data

--- selenium-test/main.py
+++ selenium-test/main.py
@@ -37,34 +37,31 @@ print('page current URL: ', driver.current_url)
 time.sleep(5)
 iframe = driver.find_element('xpath', '//*[@id="attentive_creative"]')
 driver.switch_to.frame(iframe)
-# print(driver.page_source)
 
 driver.find_element('xpath', '/html/body/div[2]/div/main/div/div/div/div[1]/div/form/div[2]/button[2]').click()
 
+driver.switch_to.default_content()
 
+time.sleep(3)
+# accessing load button so we can reach end of page before getting all of our data
+lm_button = driver.find_element('xpath' ,'//*[@id="portal-collection"]/section/button')
 
+for i in range(0,11):
+  time.sleep(2)
+  driver.execute_script("arguments[0].scrollIntoView();", lm_button)
+  print('clicked')
+  lm_button.click()
 
+#to add delay (useful for waiting for server/api loading)
+time.sleep(3)
 
-# time.sleep(3)
-# # accessing load button so we can reach end of page before getting all of our data
-# lm_button = driver.find_element('xpath' ,'//*[@id="portal-collection"]/section/button')
+# using the find_element method to find elements. You can search by name, class, etc
+# more details: https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
+elem = driver.find_elements(By.ID, "product-schema")
+for i in elem: 
+  print(i.get_attribute('innerHTML'))
 
-# for i in range(0,11):
-#   time.sleep(2)
-#   driver.execute_script("arguments[0].scrollIntoView();", lm_button)
-#   print('clicked')
-#   lm_button.click()
-
-# to add delay (useful for waiting for server/api loading)
-# time.sleep(3)
-
-# # using the find_element method to find elements. You can search by name, class, etc
-# # more details: https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
-# elem = driver.find_elements(By.ID, "product-schema")
-# for i in elem: 
-#   print(i.get_attribute('innerHTML'))
-
-# print('amount of items:', len(elem))
+print('amount of items:', len(elem))
 
 # now we can send keys (like entering keys on our keyboard)
 # first we will clear any prepoluated text (just in case), then we will pass in the query we want, and then return it
