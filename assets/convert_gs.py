c8290d2cd92b781611e2e1a1e501c6096beb319f
allonahmed <allonahmed@mec.science>
2022-10-02 19:14:48 -0400

    now takes the response instead of file

--- assets/convert_gs.py
+++ assets/convert_gs.py
@@ -2,26 +2,25 @@ import json
 
 # file for cleaning and converting json data scraped from gymshark
 
-def convert_data(path):
+def convert_data(content):
   try:
-    with open(path, "r") as file:
-      data = json.loads(file.read())
-      results = []
-      for i in data:
-        # converts each element into dict for parsing
-        x = json.loads(i)
-        # data we want to send to our db
-        dict = {
-          'organization': x.get('offers').get('seller').get('name'),
-          'brand': x.get('brand').get('name'),
-          'url': x.get('offers').get('url'),
-          'img_url': x.get('image'),
-          'price': x.get('offers').get('price'),
-          'availability': x.get('offers').get('availability'),
-          'sku': x.get('sku'),
-          'description': x.get('description')
-        }
-        results.append(dict)
-    return results
-  except IOError:
-    return 'cannot find file'
+    data = json.loads(content)
+    results = []
+    for i in data:
+      # converts each element into dict for parsing
+      x = json.loads(i)
+      # data we want to send to our db
+      dict = {
+        'organization': x.get('offers').get('seller').get('name'),
+        'brand': x.get('brand').get('name'),
+        'url': x.get('offers').get('url'),
+        'img_url': x.get('image'),
+        'price': x.get('offers').get('price'),
+        'availability': x.get('offers').get('availability'),
+        'sku': x.get('sku'),
+        'description': x.get('description')
+      }
+      results.append(dict)
+  return results
+  except ValueError:
+    return 'error'
