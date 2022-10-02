b4a1009fdb53ff6d916a3739b3ea577b85e7b81b
allonahmed <allonahmed@mec.science>
2022-10-02 15:42:03 -0400

    example sql connection function working

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 from flask import Flask
 from flask_cors import CORS, cross_origin # prevent cors policy blocks
 import logging # useful for logging info
@@ -15,14 +15,24 @@ logger = logging.getLogger('HELLO WORLD')
 # flask server instance
 app = Flask(__name__)
 
+# setup db configuruations
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'scrape_data'
+
 # mysql db instance
-mysql = MySQL()
-mysql.init_app(app)
+mysql = MySQL(app)
 
 # test function
 @app.route('/', methods=['GET'])
 def hello_world():
-  return 'hello world'
+  # create cursor connection
+  cursor = mysql.connection.cursor()
+  cursor.execute(''' INSERT INTO scrape_data.gym_shark VALUES (%s,%s)''', (1, 'Allon'))
+  mysql.connection.commit()
+  cursor.close()
+  return f"Done!!"
 
 if __name__ == "__main__":
     app.secret_key = os.getenv('SECRET_KEY', os.urandom(24))
